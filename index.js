265e49a6f2f61417ca612b731620d5ceb622f799
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2020-05-31 15:47:35 +0100

    Create a renderer class and draw map template

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const canvas = document.getElementById("canvas")
-canvas.height = 640;
-canvas.width = 640;
+const canvas = document.getElementById("canvas");
 
-const ctx = canvas.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+class Renderer {
+    constructor(canvas) {
+        this.canvas = canvas;
+        canvas.height = 680;
+        canvas.width = 740;
+        this.ctx = canvas.getContext("2d");
+    }
+
+    render() {
+        this.ctx.fillStyle = "";
+        this.ctx.fillRect(20, 20, 640, 640);
+    }
+}
+
+
+const renderer = new Renderer(canvas);
+renderer.render();
