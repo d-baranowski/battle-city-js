c8b8c148415ed0888f901df1124829812bf79664
jacekjastrzebski <jastrzebski.jacek@gmail.com>
2020-05-31 16:11:40 +0100

    attempt to draw a tank

--- index.js
+++ index.js
@@ -6,14 +6,37 @@ class Renderer {
         canvas.height = 680;
         canvas.width = 740;
         this.ctx = canvas.getContext("2d");
+        this.spriteSheet = new Image();
+        this.spriteSheet.src = "sprites.png";
+        this.spriteWidth  = 400;
+        this.spriteHeight = 256;
     }
 
     render() {
         this.ctx.fillStyle = "";
         this.ctx.fillRect(20, 20, 640, 640);
+        this.drawTank();
+        
     }
+
+    drawTank() {
+        this.ctx.drawImage(this.spriteSheet,
+                          20,
+                          20,
+                          20,
+                          20,
+                          20,
+                          20,
+                          this.spriteWidth,
+                          this.spriteHeight);
+    }
+  
 }
+
+
 
 
 const renderer = new Renderer(canvas);
 renderer.render();
+
+
