362e6c1edd3446111ffce7156b0e66fb1cd93da2
jacekjastrzebski <jastrzebski.jacek@gmail.com>
2020-06-02 21:49:43 +0100

    keyControlls added, tanks moves and collides with maps borders

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import SpriteSheet from "./SpriteSheet";
 import Renderer from "./Renderer";
 import Tank from "./Tank";
+import KeyControls from "./KeyControls"
+
+
+
 
 async function init() {
     const spriteSheet = new SpriteSheet();
     const canvas = document.getElementById("canvas");
+    const controls = new KeyControls();
     const tank = new Tank();
 
     const renderer = new Renderer(canvas, spriteSheet);
     await renderer.loaded();
 
-    const speed = 64;
+    const speed = 200;
     let p1 = new Tank();
-    let p2 = new Tank();
 
-    p1.y = 40
-    p2.y = 70
+    p1.y = 400
 
     let dt = 0;
     let last = 0;
@@ -27,13 +30,25 @@ async function init() {
         last = t;
 
         // Game logic code
-
-        p1.x += speed * dt;
-        p2.x += speed * (1 / 60);
-        if (p1.x > 500) p1.x -= 500 + 50;
-        if (p2.x > 500) p2.x -= 500 + 50;
-
-        renderer.render([p1, p2])
+        if (controls.x == 1) {
+            p1.x += speed * dt;  
+        }
+        if (controls.x == -1) {
+            p1.x -= speed * dt;  
+        }
+        if (controls.y == 1) {
+            p1.y += speed * dt;  
+        }
+        if (controls.y == -1) {
+            p1.y -= speed * dt;  
+        }
+            
+        if (p1.x >= 640 - 20) p1.x = 640 - 20;
+        if (p1.x <= 20)  p1.x = 20;
+        if (p1.y >= 640 - 20) p1.y = 640 - 20;
+        if (p1.y <= 20)  p1.y = 20;
+        
+        renderer.render([p1])
 
     }
     requestAnimationFrame(loopy);
