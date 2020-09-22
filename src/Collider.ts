19814d3b75acd132bd898ff572b399bf7c3d9263
Dan <Daniel.baranowski@infinityworks.com>
2020-09-22 20:31:16 +0100

    Update src/Collider.ts
    Co-authored-by: jacekjastrzebski <jastrzebski.jacek@gmail.com>

--- src/Collider.ts
+++ src/Collider.ts
@@ -13,7 +13,7 @@ class Collider {
     }
 
     static overlaps(o1: IGameObject, o2: IGameObject): boolean {
-        return !!(o1.x + o1.width >= o2.x &&
+        return !(o1.x + o1.width >= o2.x &&
             o1.x <= o2.x + o2.width &&
             o1.y + o1.height >= o2.y &&
             o1.y <= o2.y + o2.height);
